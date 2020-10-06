0410eeeb89c3efd8f1df1b3c34ce405d207c5757
NazarK <naaazar800@gmail.com>
2020-10-06 23:26:05 +0300

    addForm

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import {SignUp} from "./siginUp/SignUp";
+
 const App = () => {
     return (
         <Router>
             <Switch>
-                <Route path="" exact component={() => <div>Main Page</div>} />
+                <Route path="/" exact component={() => <div>Main Page</div>} />
+                <Route  path="/signUp" exact component={() => <SignUp/>} />
                 <Route component={() => <h1 style={{ textAlign: 'center' }}>404 not found</h1>} />
             </Switch>
         </Router>
