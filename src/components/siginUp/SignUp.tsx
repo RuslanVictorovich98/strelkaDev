e54a5bbf46c1445ac00a593166c68417d1fbdcd6
NazarK <naaazar800@gmail.com>
2020-10-07 23:01:22 +0300

    added  registration form

--- src/components/siginUp/SignUp.tsx
+++ src/components/siginUp/SignUp.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import {Formik} from 'formik';
+import {Formik, Field} from 'formik';
 import './SignUp.css'
+import {Button} from 'antd';
+import 'antd/dist/antd.css'
+import {initialState} from "../../store/reducers/training";
 // export const SignUp = () => {
 //     return <div>
 //         <p>registration Form</p>
@@ -10,9 +13,9 @@ import './SignUp.css'
 
 export const SignUp = () => (
     <div className={"formWrapper"}>
-        <h1>Anywhere in your app!</h1>
+        <h1 className="titleForm">Регистрация</h1>
         <Formik
-            initialValues={{email: '', password: ''}}
+            initialValues={{name: '', surname: '', gender: "mean", email: '', password: ''}}
             validate={values => {
                 // const errors = {};
                 // if (!values.email) {
@@ -30,6 +33,7 @@ export const SignUp = () => (
                     setSubmitting(false);
                 }, 400);
             }}
+
         >
             {({
                   values,
@@ -38,45 +42,64 @@ export const SignUp = () => (
                   handleChange,
                   handleBlur,
                   handleSubmit,
+                  initialValues,
                   isSubmitting,
                   /* and other goodies */
               }) => (
                 <form onSubmit={handleSubmit}>
 
                     <div className={"formBodyWrapper"}>
-                        <label htmlFor="name">name</label>
+                        <label htmlFor="name">Name</label>
                         <input
+                            className="formInput"
                             type="text"
                             name="name"
                             onChange={handleChange}
                             onBlur={handleBlur}
-                            value={values.email}
+                            value={values.name}
                         />
-                        <label htmlFor="surname">surname</label>
+                        <label htmlFor="surname">Surname</label>
                         <input
+                            className="formInput"
                             type="surname"
                             name="surname"
                             onChange={handleChange}
                             onBlur={handleBlur}
-                            value={values.email}
+                            value={values.surname}
                         />
-                        <label htmlFor="email">gender</label>
-                        <input
-                            type="radio"
-                            name="gender"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                        />
-                        <input
-                            type="radio"
-                            name="gender"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                        />
+                        <div className="genderInput">
+                            <label htmlFor="email">Gender</label>
+                            <span>
+                                <span>Мужской</span>
+                            {/*<input*/}
+                            {/*    className="formInput"*/}
+                            {/*    type="radio"*/}
+                            {/*    name="gender"*/}
+                            {/*    onClick={() => {values.gender = true}}*/}
+                            {/*    onBlur={handleBlur}*/}
+                            {/*    checked={values.gender || initialValues.gender}*/}
+                            {/*    // value={values.gender}*/}
+                            {/*/>*/}
+                                <Field type="checkbox" name="gender" value = "mean" />
+                            </span>
+                            <span>
+                              <span>женский</span>
+                                {/*<input*/}
+                                {/*    className="formInput"*/}
+                                {/*    type="radio"*/}
+                                {/*    name="gender"*/}
+                                {/*    onClick={() => {console.log('нажав');values.gender = false}}*/}
+                                {/*    onBlur={handleBlur}*/}
+                                {/*    checked ={values.gender || initialValues.gender}*/}
+                                {/*    // value={values.email}*/}
+                                {/*/>*/}
+                                <Field type="checkbox" name="gender" value = "female" />
+                          </span>
+
+                        </div>
                         <label htmlFor="surname">Email Address</label>
                         <input
+                            className="formInput"
                             type="text"
                             name="email"
                             onChange={handleChange}
@@ -84,8 +107,9 @@ export const SignUp = () => (
                             value={values.email}
                         />
                         {errors.email && touched.email && errors.email}
-                        <label htmlFor="password">password</label>
+                        <label htmlFor="password">Password</label>
                         <input
+                            className="formInput"
                             type="password"
                             name="password"
                             onChange={handleChange}
@@ -93,9 +117,11 @@ export const SignUp = () => (
                             value={values.password}
                         />
                         {errors.password && touched.password && errors.password}
-                        <button type="submit" disabled={isSubmitting}>
-                            Submit
-                        </button>
+                        {/*<button type="submit" disabled={isSubmitting}>*/}
+                        {/*    Submit*/}
+                        {/*</button>*/}
+                        <Button type="primary" onClick={() => handleSubmit()}
+                                className="formButton">Зарегистрировать</Button>
                     </div>
                 </form>
 
